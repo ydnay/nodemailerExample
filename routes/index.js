b2e4d0285bc0955a206fd58c6180ee07fbccf98d
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-30 13:29:34 -0400

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const nodemailer = require('nodemailer');
+const templates = require('../templates/templates');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('email');
+});
+
+// router.post('/send-email', (req, res, next) => {
+//   const { email, subject, message } = req.body;
+//   res.render('message', { email, subject, message })
+// });
+
+router.post('/send-email', (req, res, next) => {
+  let { email, subject, message } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAILACC,
+      pass: process.env.EMAILPASS,
+    }
+  });
+  transporter.sendMail({
+    from: 'yandygonzalez@gmail.com',
+    to: email, 
+    subject: subject, 
+    html: templates.templateExample2(message),
+  })
+  .then(info => res.render('message', {email, subject, message, info}))
+  .catch(error => console.log(error));
 });
 
 module.exports = router;
